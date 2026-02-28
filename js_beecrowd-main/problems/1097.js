4338122fbd39356bb4c982286ef00f08761c1761
Dihêgo Pires <dihegopires@gmail.com>
2026-02-28 16:12:13 -0300

    feact(1097): Sequencia ICJ3

---
+++ js_beecrowd-main/problems/1097.js
@@ -0,0 +1,7 @@
+export function problem() {
+    for (let i = 1; i <= 9; i+=2) {
+        for (let j = i + 6; j >= i + 4; j--){
+            console.log(`I=${i} J=${j}`)            
+        }
+    }
+}
