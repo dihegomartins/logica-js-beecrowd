005c00a8297fdb108898d7ea0753d2140a074c32
Dihêgo Pires <dihegopires@gmail.com>
2026-03-10 11:48:03 -0300

    feat(1183): Acima da Diagonal Principal

---
+++ js_beecrowd-main/problems/1183.js
@@ -0,0 +1,29 @@
+export function problem(lines) {
+    let aux = 0
+    let operacao = lines[aux++].trim()
+    let soma = 0
+    let media = 0
+    let matriz = []
+
+    for (let i = 0; i < 12; i++) {
+        matriz[i] = [];
+
+        for (let j = 0; j < 12; j++) {
+            let x = Number(lines[aux++])
+            matriz[i][j] = x
+        }
+    }
+
+    for (let i = 0; i < 12; i++) {
+        for (let j = i+1; j < 12; j++) {
+            soma += matriz[i][j]
+        }
+    }
+
+    if(operacao === 'M'){
+        media = soma/66
+        console.log(`${media.toFixed(1)}`)
+    } else {
+        console.log(`${soma.toFixed(1)}`)
+    }
+}
