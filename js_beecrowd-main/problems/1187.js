b70113bb825e2af0827780a6e6fba533cc2a1c52
Dihêgo Pires <dihegopires@gmail.com>
2026-03-10 15:51:05 -0300

    feat(1187): Área Superior

---
+++ js_beecrowd-main/problems/1187.js
@@ -0,0 +1,34 @@
+export function problem(lines) {
+    let aux = 0
+    let operacao = lines[aux++].trim()
+    let soma = 0
+    let media = 0
+    let matriz = []
+
+    for (let i = 0; i < 12; i++) {
+        matriz[i] = [];
+
+        for (let j = 0; j < 12; j++) {
+            let x = Number(lines[aux++])
+            matriz[i][j] = x
+        }
+    }
+
+    let tam = 12
+    let metade = tam/2
+    let cont = 0
+
+    for (let i = 0; i < metade; i++) {
+        for (let j = i + 1; j < 11 - i; j++) {
+            soma += matriz[i][j]
+            cont++;
+        }
+    }
+
+    if(operacao === 'M'){
+        media = soma/cont
+        console.log(`${media.toFixed(1)}`)
+    } else {
+        console.log(`${soma.toFixed(1)}`)
+    }
+}
